2dda7b50d4cca2d7e5ee4a9d43576eec67de3594
Jimmy <jimmyleespann@outlook.com>
2026-01-18 01:46:46 -0500

    create card layout

--- src/card-game/card-game.jsx
+++ src/card-game/card-game.jsx
@@ -4,31 +4,37 @@ import { cards } from '../card-game/card-data.js';
 import './card-game.css'; // Assume your styles are here
 
 const CardGame = () => {
+    const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
 
     return (
         <div
-            style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                display: 'flex',
-            }}
+            className="card-game-container"
         >
-            {cards.map((card, index) => (
-                <Card
-                    key={index}
-                    name={card.name}
-                    type={card.type}
-                    image={card.image}
-                    description={card.description}
-                    evasion={card.evasion}
-                    defense={card.defense}
-                    health={card.health}
-                    elements={card.elements}
-                    passives={card.passives}
-                    actions={card.actions}
-                />
-            ))}
-        </div>
+            <div className="card-layout">
+                {cards.map((card, index) => (
+                    <div
+                        className={`card-game-card ${hoveredCardIndex === index ? 'card-hover' : ''}`}
+                        onMouseEnter={() => setHoveredCardIndex(index)}
+                        onMouseLeave={() => setHoveredCardIndex(null)}
+                        key={index}
+                    >
+                        <Card
+                            key={index}
+                            name={card.name}
+                            type={card.type}
+                            image={card.image}
+                            description={card.description}
+                            evasion={card.evasion}
+                            defense={card.defense}
+                            health={card.health}
+                            elements={card.elements}
+                            passives={card.passives}
+                            actions={card.actions}
+                        />
+                    </div>
+                ))}
+            </ div>
+        </div >
     );
 };
 
