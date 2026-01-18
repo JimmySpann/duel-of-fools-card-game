bed1928b7c1ed4ff4720877d08d2c7bff699198d
Jimmy <jimmyleespann@outlook.com>
2026-01-18 03:23:32 -0500

    add new element icons, add new stats, css fixes for card

--- src/card-game/card.jsx
+++ src/card-game/card.jsx
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
-import fireIcon from '../assets/elements/fire-icon.png';
+
 import cardBack from '../assets/card-back.jpg';
+import fireIcon from '../assets/elements/fire-icon.png';
+import iceIcon from '../assets/elements/ice-icon.png';
+import earthIcon from '../assets/elements/earth-icon.png';
+import airIcon from '../assets/elements/air-icon.png';
+import electricIcon from '../assets/elements/lightning-icon.png';
+import waterIcon from '../assets/elements/water-icon.png';
+import deathIcon from '../assets/elements/death-icon.png';
+
 import './card.css';
-// import waterIcon from '../assets/elements/water-icon.png';
-// import earthIcon from '../assets/elements/earth-icon.png';
-// import airIcon from '../assets/elements/air-icon.png';
 
 const CardHeader = ({ type, elements }) => {
+    const getElementIcon = (element) => {
+        switch (element) {
+            case 'fire':
+                return fireIcon;
+            case 'ice':
+                return iceIcon;
+            case 'earth':
+                return earthIcon;
+            case 'air':
+                return airIcon;
+            case 'electric':
+                return electricIcon;
+            case 'water':
+                return waterIcon;
+            case 'death':
+                return deathIcon;
+            // Add cases for other elements like earth, air, etc.
+            default:
+                return null;
+        }
+    };
+
     const processElements = () => {
         let elementArray = [];
         for (const [key, value] of Object.entries(elements)) {
@@ -27,12 +54,12 @@ const CardHeader = ({ type, elements }) => {
 
             <div className='card-elements'>
                 {elementArray.map((type, index) => (
-                    <div className="card-elements-icon">
-                        <img src={fireIcon} className="card-elements-icon-image" />
+                    <div className="card-elements-icon" key={index}>
+                        <img src={getElementIcon(type)} className="card-elements-icon-image" />
                     </div>
                 ))}
                 {normalCount > 0 && (
-                    <div className="card-elements-icon" style={{ backgroundColor: 'gray' }}>
+                    <div className="card-elements-icon" style={{ backgroundColor: 'gray', textAlign: 'center', fontWeight: 'bold' }}>
                         {normalCount}
                     </div>
                 )}
@@ -41,15 +68,26 @@ const CardHeader = ({ type, elements }) => {
     )
 }
 
-const Card = ({ name, type, elements, image, description, passives, actions, evasion, defense, health }) => {
-    const [isActive, setIsActive] = useState(false);
-
+const Card = ({
+    name,
+    type,
+    elements,
+    image,
+    description,
+    passives,
+    actions,
+    evasion,
+    defense,
+    attack,
+    agility,
+    health,
+    isFlipped
+}) => {
     return (
         <div
             className="card-container"
-            onClick={() => setIsActive(!isActive)}
         >
-            <div className={`card-front ${isActive ? 'rotateY-180' : ''}`}>
+            <div className={`card-front ${isFlipped ? 'rotateY-180' : ''}`}>
                 <CardHeader type={type} elements={elements} />
 
                 <div className="card-image-container">
@@ -78,13 +116,15 @@ const Card = ({ name, type, elements, image, description, passives, actions, eva
                 </div>
 
                 <div className="card-footer">
-                    <div>Defense: {defense}</div>
-                    <div>Health: {health}</div>
-                    <div>Evasion: {evasion}</div>
+                    <div>ATK:{attack}</div>
+                    <div>AGI:{agility}</div>
+                    <div>HP:{health}/{health}</div>
+                    <div>EVA:{evasion}</div>
+                    <div>DEF:{defense}</div>
                 </div>
             </div>
 
-            <div className={`card-back ${isActive ? 'rotateY-0' : ''}`}>
+            <div className={`card-back ${isFlipped ? 'rotateY-0' : ''}`}>
                 <img
                     src={cardBack}
                     className="card-back-image"
