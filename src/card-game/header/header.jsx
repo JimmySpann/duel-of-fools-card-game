fa95270eb0352971a8209277fffee5ddb9a7a73a
Jimmy <jimmyleespann@outlook.com>
2026-02-03 21:04:58 -0500

    add header ui

--- src/card-game/header/header.jsx
+++ src/card-game/header/header.jsx
@@ -5,14 +5,26 @@ const Header = ({ }) => {
     return (
         <div className="header-container">
             <h2 className="header-title">
-                Card Game
+                Duel of Fools
             </h2>
-            <button
-                className="button"
-            // onClick={onFlipAllCards}
-            >
-                Flip All Cards
-            </button>
+            <div className="account-buttons-container">
+                <button className="account-button">Switch <br /> Games</button>
+                <button className="account-button">Sign <br /> Out</button>
+            </div>
+            <div className="player-container">
+                <div className="player-buttons-container">
+                    <button className="player-button">Brief</button>
+                    <button className="player-button">Chat</button>
+                    <button className="player-button">Hand</button>
+                </div>
+                <div className="player-card-container">
+                    <div className="player-name-card">
+                        <h2 className="player-name-title">
+                            Your Turn
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
